ecbe4bcf0612b3e8679f799a32134e639c2af20a
Mattiatarabolo <mattia.tarabolo@gmail.com>
2024-10-04 14:29:49 -0400

    Correct update functions

--- src/population_dynamics.jl
+++ src/population_dynamics.jl
@@ -28,11 +28,19 @@ end
 
 # Placeholder update functions, where you can optimize the internal logic of f_mu, f_q, f_chi
 function f_μ(sum_χ::Float64, Ε::Float64, Δ::Float64, φ::Float64, Φ::Float64)
-    return Ε / (1 - sum_χ) * (Δ * Φ + φ )
+    if sumχ < 1.0
+        return Ε / (1 - sum_χ) * (Δ * Φ + φ)
+    else
+        return Ε / (1 - sum_χ) * (Δ * Φ - φ)
+    end
 end
 
 function f_q(sum_q::Float64, sum_χ::Float64, Δ::Float64, φ::Float64, Φ::Float64)
-    return sum_q / (1 - sum_χ)^2 * ((1+Δ^2) * Φ + Δ * φ)
+    if sumχ < 1.0
+        return sum_q / (1 - sum_χ)^2 * ((1+Δ^2) * Φ + Δ * φ)
+    else
+        return sum_q / (1 - sum_χ)^2 * ((1+Δ^2) * Φ - 3 * Δ * φ)
+    end
 end
 
 function f_χ(sum_χ::Float64, Φ::Float64)
