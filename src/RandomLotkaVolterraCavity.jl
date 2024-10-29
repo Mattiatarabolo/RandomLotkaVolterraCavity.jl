eb6c8a2d99a0b87f8b69a6460ac052f0189be9ed
Mattiatarabolo <mattia.tarabolo@gmail.com>
2024-10-29 15:59:35 +0100

    New FC method

--- src/RandomLotkaVolterraCavity.jl
+++ src/RandomLotkaVolterraCavity.jl
@@ -1,7 +1,7 @@
 module RandomLotkaVolterraCavity
     using Random, StatsBase, SpecialFunctions, ProgressMeter
 
-    export population_dynamics
+    export population_dynamics, population_dynamics_FC
 
     include("utils.jl")
     include("population_dynamics.jl")
--- src/population_dynamics.jl
+++ src/population_dynamics.jl
@@ -148,17 +148,97 @@ function population_dynamics(p_k::Vector{Float64}, p_cav_k::Vector{Float64}, P::
     return μ_full_population, q_full_population, χ_full_population, μ_cav_population, q_cav_population, χ_cav_population
 end
 
-#=
-# Example usage
-P = Int(1e4)  # Population size
-K = 4  # Average degree of the graph
-k_max = 19
-p_k = [(k+1)*exp(-K)*K^k/factorial(k+1) for k in 1:k_max]  # This is the excess degree distribution, i.e. the cavity degree distribution of an Erdos-Renyi random graph
-tol = 1e-5  # Convergence tolerance
-max_iter = Int(1e6)  # Maximum number of iterations
-m = 0.0  # Mean coupling
-σ = 0.1  # Standard deviation of coupling
-γ = -0.1  # Correlation coefficient between J and J'
+# Function to compute all the quantities obtain by the sum of neighbours' terms in the cavity update
+function sumpop_FC(μ_population::Vector{Float64}, q_population::Vector{Float64}, χ_population::Vector{Float64}, J_population::Vector{Float64}, J_prime_population::Vector{Float64}, i::Int, P::Int)
+    sum_μ = 0.0
+    sum_q = 0.0
+    sum_χ = 0.0
+    @inbounds @fastmath @simd for j in 1:P
+        if j == i
+            continue
+        end
+        sum_μ += J_population[j] * μ_population[j]
+        sum_q += J_population[j]^2 * q_population[j]
+        sum_χ += J_population[j] * J_prime_population[j] * χ_population[j]
+    end
+    Ε = sqrt(sum_q)
+    Δ = (1+sum_μ)/Ε
 
-μ_population, q_population, χ_population = population_dynamics(p_k, P, tol, max_iter, m, σ, γ, K)
-=#
+    testvalues(sum_μ, sum_q, sum_χ, Ε, Δ)
+    return sum_q, sum_χ, Ε, Δ
+end
+
+# Function to run population dynamics with all the performance tips included
+function population_dynamics_FC(P::Int, tol::Float64, max_iter::Int, m::Float64, σ²::Float64, γ::Float64; check_conv=30, rng=Xoshiro(1234), verbose=false)
+    # Initialize populations
+    q_population = rand(rng, P)
+    χ_population = rand(rng, P)
+    J_population = zeros(P)
+    J_prime_population = zeros(P)
+
+    # Initialize averages to check convergence
+    avg_μ = 0.0
+ #   avg_q = 0.0
+ #   avg_χ = 0.0
+ #   std_μ = 0.0
+ #   std_q = 0.0
+ #   std_χ = 0.0
+
+    # Loop over iterations until convergence or max_iter is reached
+    converged = false
+    @showprogress for t in 1:max_iter
+
+        # Update each site in the population
+        @inbounds for i in shuffle(rng, 1:P)
+            @inbounds for j in 1:P
+                if j == i
+                    continue
+                end
+                J_population[j], J_prime_population[j] = sample_couplings(rng, m, σ², γ, P)
+            end
+            # Compute the new values for mu, q, chi using the update functions
+            sum_q, sum_χ, Ε, Δ = sumpop_FC(μ_cav_population, q_cav_population, χ_cav_population, J_population, J_prime_population, i, P)
+            μ_population[i] = f_μ(sum_χ, Ε, Δ)
+            q_population[i] = f_q(sum_q, sum_χ, Δ)
+            χ_population[i] = f_χ(sum_χ, Δ)
+            testvalues(μ_population[i], q_population[i], χ_population[i], sum_q, sum_χ, Δ)  
+        end
+
+        # Calculate new averages for convergence checking
+        new_avg_μ = mean(μ_population)
+     #   new_avg_q = mean(q_cav_population)
+     #   new_avg_χ = mean(χ_cav_population)
+     #   new_std_μ = std(μ_population; mean=new_avg_μ)
+     #   new_std_q = std(q_population; mean=new_avg_q)
+     #   new_std_χ = std(χ_population; mean=new_avg_χ)
+
+        # Calculate the maximum change in the updates for convergence checking
+        max_diff = abs(avg_μ - new_avg_μ)
+        #max_diff = max(max_diff, abs(avg_q - new_avg_q))
+        #max_diff = max(max_diff, abs(avg_χ - new_avg_χ))
+    #    max_diff = max(max_diff, abs(std_μ - new_std_μ))
+    #    max_diff = max(max_diff, abs(std_q - new_std_q))
+    #    max_diff = max(max_diff, abs(std_χ - new_std_χ))
+        avg_μ = new_avg_μ
+    #    avg_q = new_avg_q
+    #    avg_χ = new_avg_χ
+
+        # Check for convergence
+        if t % check_conv == 0  # Check every 10 iterations
+            if verbose
+                println("Iteration $t: max_diff = $max_diff")
+            end
+            if max_diff < tol
+                converged = true
+                println("Converged after $t iterations.")
+                break
+            end
+        end
+    end
+
+    if !converged
+        println("Reached max iterations without convergence.")
+    end
+
+    return μ_population, q_population, χ_population, μ_population, q_population, χ_population
+end
