22401b6db1f1960aad25d87f81abcbfca2217a62
Mattiatarabolo <mattia.tarabolo@gmail.com>
2025-01-16 11:34:40 +0100

    Export sample function in RandomLotkaVolterraCavity module

--- src/RandomLotkaVolterraCavity.jl
+++ src/RandomLotkaVolterraCavity.jl
@@ -1,7 +1,7 @@
 module RandomLotkaVolterraCavity
     using Random, StatsBase, SpecialFunctions, ProgressMeter, SparseArrays, Graphs, OrdinaryDiffEq, LinearAlgebra
 
-    export population_dynamics, population_dynamics_FC
+    export population_dynamics, population_dynamics_FC, sample
 
     include("utils.jl")
     include("population_dynamics.jl")
