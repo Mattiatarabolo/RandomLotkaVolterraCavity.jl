d32924b08f70fc4f51d90b2055e840d82d834a58
Mattiatarabolo <mattia.tarabolo@gmail.com>
2025-01-17 15:06:29 +0100

    Update CI configuration and project dependencies for Julia 1.9 compatibility

--- src/RandomLotkaVolterraCavity.jl
+++ src/RandomLotkaVolterraCavity.jl
@@ -1,7 +1,7 @@
 module RandomLotkaVolterraCavity
     using Random, StatsBase, SpecialFunctions, ProgressMeter, SparseArrays, Graphs, OrdinaryDiffEq, LinearAlgebra, PyPlot, PyCall
 
-    export population_dynamics, population_dynamics_FC, sample, sample_couplings
+    export population_dynamics, population_dynamics_FC, sample_glv, sample_couplings
 
     include("utils.jl")
     include("population_dynamics.jl")
