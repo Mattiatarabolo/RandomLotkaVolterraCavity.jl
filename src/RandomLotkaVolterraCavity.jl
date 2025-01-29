b3b0780bfa962f8f9bc7fedfd3b591300b2ff125
Mattiatarabolo <mattia.tarabolo@gmail.com>
2025-01-29 15:46:49 +0100

    Add get_index function to RandomLotkaVolterraCavity module exports for improved accessibility

--- src/RandomLotkaVolterraCavity.jl
+++ src/RandomLotkaVolterraCavity.jl
@@ -1,7 +1,7 @@
 module RandomLotkaVolterraCavity
     using Random, StatsBase, SpecialFunctions, ProgressMeter, SparseArrays, Graphs, OrdinaryDiffEq, LinearAlgebra, PyPlot, PyCall
 
-    export population_dynamics, population_dynamics_FC, sample_glv, sample_couplings, sample_x, PdfDegVec, sample_degree
+    export population_dynamics, population_dynamics_FC, sample_glv, sample_couplings, sample_x, PdfDegVec, sample_degree, get_index
 
     include("utils.jl")
     include("population_dynamics.jl")
